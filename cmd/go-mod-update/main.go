28f2f5c9ddf8e153cb95c65eb42e9f29d545c4a6
Kevin C. Krinke <kevin@krinke.ca>
2024-01-16 13:43:59 -0500

    cmd: remove deprecated Include* cdk build flags

--- cmd/go-mod-update/main.go
+++ cmd/go-mod-update/main.go
@@ -19,8 +19,6 @@ import (
 
 	"github.com/urfave/cli/v2"
 
-	"github.com/go-curses/cdk"
-	cstrings "github.com/go-curses/cdk/lib/strings"
 	"github.com/go-curses/cdk/log"
 
 	"github.com/go-curses/coreutils-go-mod-update/ui"
@@ -34,37 +32,10 @@ const (
 	APP_DESCRIPTION = "command line utility for updating golang dependencies"
 )
 
-// Build Configuration Flags
-// setting these will enable command line flags and their corresponding features
-// use `go build -v -ldflags="-X 'main.IncludeLogFullPaths=false'"`
-var (
-	IncludeProfiling          = "false"
-	IncludeLogFile            = "false"
-	IncludeLogFormat          = "false"
-	IncludeLogFullPaths       = "false"
-	IncludeLogLevel           = "false"
-	IncludeLogLevels          = "false"
-	IncludeLogTimestamps      = "false"
-	IncludeLogTimestampFormat = "false"
-	IncludeLogOutput          = "false"
-)
-
 var (
 	BuildVersion = "v0.0.0"
 	BuildRelease = "trunk"
 )
-
-func init() {
-	cdk.Build.Profiling = cstrings.IsTrue(IncludeProfiling)
-	cdk.Build.LogFile = cstrings.IsTrue(IncludeLogFile)
-	cdk.Build.LogFormat = cstrings.IsTrue(IncludeLogFormat)
-	cdk.Build.LogFullPaths = cstrings.IsTrue(IncludeLogFullPaths)
-	cdk.Build.LogLevel = cstrings.IsTrue(IncludeLogLevel)
-	cdk.Build.LogLevels = cstrings.IsTrue(IncludeLogLevels)
-	cdk.Build.LogTimestamps = cstrings.IsTrue(IncludeLogTimestamps)
-	cdk.Build.LogTimestampFormat = cstrings.IsTrue(IncludeLogTimestampFormat)
-	cdk.Build.LogOutput = cstrings.IsTrue(IncludeLogOutput)
-}
 
 func main() {
 	updater := ui.NewUI(
